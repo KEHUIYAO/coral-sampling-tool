a7fdc4c8e1fb48f673db68de2bd7f6647ac59a23
KEHUI YAO <ykhray19960527@gmail.com>
2022-04-20 23:34:43 -0500

    ddd

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from main import app
 from layouts import layout
 from callbacks import callback
 server = app.server  # if using heroku
+app.layout = layout.generate_layout()
 
 # Run app and display result inline in the notebook
 if __name__ == '__main__':
-    app.layout = layout.generate_layout()
     #app.run_server(debug=True, host='127.0.0.1')
     app.run_server(debug=True)  # if using heroku
